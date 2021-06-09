557940ec040b3429977b003ef56ff280b1d7e5bd
lahlim <lassi_mustonen@hotmail.com>
2021-06-09 23:58:35 +0300

    more commands added. Motivation and hello command.

--- commands/BasicCommands.cs
+++ commands/BasicCommands.cs
@@ -2,12 +2,13 @@ using System;
 
 using System.Threading.Tasks;
 using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
 using DSharpPlus.CommandsNext.Attributes;
 
 public class BasicCommandModule : BaseCommandModule
 {
     [Command("greet")]
-    public async Task GreedCommand(CommandContext ctx)
+    public async Task GreetCommand(CommandContext ctx)
     {
         await ctx.RespondAsync("Greetings! Thank you for executing me!");
     }
@@ -29,6 +30,35 @@ public class BasicCommandModule : BaseCommandModule
         return curese;
     }
 
+    [Command("moi")]
+    public async Task TestCommand(CommandContext ctx)
+    {
+        await ctx.RespondAsync("No terve " + ctx.User.Username + "!");
+    }
+
+    [Command("motivoi")]
+    public async Task MotivateCommand(CommandContext ctx)
+    {
+        await ctx.RespondAsync(GetMotivation(ctx.User.Username));
+    }
+    /**
+    Returns random curseword from hardcoded list
+    */
+    public static string GetMotivation(string name)
+    {
+        string[] motivations = {
+         $"Hyvää työtä {name}",
+         $"On varmasti olemassa huonompia videopelaajia kuin {name}!",
+         $"Kyllä vielä joskus löydät itsellesi jotain pelaamista järkevämpää tekemistä :)",
+         $"Jaksaa jaksaa {name}!",
+         $"No aina voisi olla asiat huonomminkin!"
+         };
+        Random rd = new Random();
+        int index = rd.Next(motivations.Length);
+        string motivation = motivations[index];
+        index = rd.Next(motivations.Length);
+        return motivation;
+    }
 }
 
 
